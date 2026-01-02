05c46de0f4cfdadbd821319ed907bbc8c547904b
WYlim1003 <cocolimwanyee@gmail.com>
2026-01-02 12:43:42 +0800

    update

--- quiz.js
+++ quiz.js
@@ -177,7 +177,7 @@ async function submitQuiz(event) {
     // Prepare payload
    const payload = {
     timestamp: new Date().toISOString(),
-    visitor_id: visitorID,
+    visitorid: visitorID,
     score: score,
     percentage: percentage,
     question1: answers.q1.toUpperCase(),
